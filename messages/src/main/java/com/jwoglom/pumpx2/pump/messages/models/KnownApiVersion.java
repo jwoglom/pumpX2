544d7f40c0990b312fd635aa21a13e9907e97a4c
James Woglom <j@wogloms.net>
2024-02-21 00:47:04 -0500

    models: add v7.7 KnownApiVersion

--- messages/src/main/java/com/jwoglom/pumpx2/pump/messages/models/KnownApiVersion.java
+++ messages/src/main/java/com/jwoglom/pumpx2/pump/messages/models/KnownApiVersion.java
@@ -6,6 +6,8 @@ public enum KnownApiVersion {
     API_V2_1(2, 1),
     // v2.5 is the API version used by software v7.6 and includes remote bolus
     API_V2_5(2, 5),
+    // v3.2 is the API version used by software v7.7 and utilizes a 6-character numeric pairing PIN
+    API_V3_2(3, 2),
     // Represents known messages from the app that we can't parse with known firmware
     API_FUTURE(99, 99)
     ;
