b05e0f722310eb6eb80702d11b1f362ddb54342d
James Woglom <j@wogloms.net>
2025-02-12 21:05:15 -0500

    fix jsonl summary

--- scripts/jsonl-summary.sh
+++ scripts/jsonl-summary.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cat "$1" | jq -c -s '.[] | select(.parsed.name) | [.raw.ts, .raw.type, .parsed.messageProps.characteristic, .parsed.name, .raw.value, .parsed.params]'
+cat "$1" | jq -c -s '
+  .[] |
+  if .error.message? then
+    [.raw.ts, .raw.type, .error.message]
+  elif .parsed.name then
+    [.raw.ts, .raw.type, .parsed.messageProps.characteristic, .parsed.name, .raw.value, .parsed.params]
+  else
+    empty
+  end
+'
