1a63c3f02fc8f89dfbc6acda3300e2fff750fa7d
James Woglom <j@wogloms.net>
2025-02-18 20:28:04 -0500

    setPairingCode in pair()

--- androidLib/src/main/java/com/jwoglom/pumpx2/pump/bluetooth/TandemPump.java
+++ androidLib/src/main/java/com/jwoglom/pumpx2/pump/bluetooth/TandemPump.java
@@ -245,9 +245,11 @@ public abstract class TandemPump {
      *                    displayed on the pump screen.
      */
     public void pair(BluetoothPeripheral peripheral, @Nullable AbstractCentralChallengeResponse centralChallenge, String pairingCode) {
-        if (PumpState.pairingCodeType == PairingCodeType.SHORT_6CHAR || deviceModel == KnownDeviceModel.MOBI) {
+        if (PumpState.pairingCodeType == PairingCodeType.SHORT_6CHAR || deviceModel == KnownDeviceModel.MOBI || pairingCode.length() == 6) {
+            PumpState.pairingCodeType = PairingCodeType.SHORT_6CHAR;
             String jpakeSecretHex = PumpState.getJpakeDerivedSecret(context);
             PumpState.setJpakeServerNonce(context, "");
+            PumpState.setPairingCode(context, pairingCode);
             if (Strings.isNullOrEmpty(jpakeSecretHex)) {
                 Timber.i("TandemPump: pair(SHORT_6CHAR, pairingCode=" + pairingCode + ", BOOTSTRAP)");
                 JpakeAuthBuilder.clearInstance();
@@ -268,6 +270,7 @@ public abstract class TandemPump {
         } else if (PumpState.pairingCodeType == PairingCodeType.LONG_16CHAR) {
             Timber.i("TandemPump: pair(LONG_16CHAR, " + pairingCode + ")");
             try {
+                PumpState.setPairingCode(context, pairingCode);
                 Message message = PumpChallengeRequestBuilder.create(centralChallenge, pairingCode);
                 sendCommand(peripheral, message);
             } catch (PumpChallengeRequestBuilder.InvalidPairingCodeFormat e) {
