f28b7da6ca3dfe4cfda7842467c2632541e28b42
James Woglom <j@wogloms.net>
2025-03-27 02:03:36 -0400

    skip test

--- messages/src/test/java/com/jwoglom/pumpx2/pump/messages/response/historyLog/DexcomG7CGMHistoryLogTest.java
+++ messages/src/test/java/com/jwoglom/pumpx2/pump/messages/response/historyLog/DexcomG7CGMHistoryLogTest.java
@@ -3,9 +3,11 @@ package com.jwoglom.pumpx2.pump.messages.response.historyLog;
 import static com.jwoglom.pumpx2.pump.messages.MessageTester.assertHexEquals;
 
 import org.apache.commons.codec.DecoderException;
+import org.junit.Ignore;
 import org.junit.Test;
+@Ignore("need sample")
 
-public class DexcomG7CGMHistoryLogHistoryLogTest {
+public class DexcomG7CGMHistoryLogTest {
     @Test
     public void testDexcomG7CGMHistoryLogHistoryLog() throws DecoderException {
         DexcomG7CGMHistoryLog expected = new DexcomG7CGMHistoryLog(
