6471e24fbaaa1a3175089a45640ba0fe09072d16
James Woglom <j@wogloms.net>
2025-12-06 21:51:54 -0500

    add missing Tandem Mobi device prefix

--- messages/src/main/java/com/jwoglom/pumpx2/pump/messages/bluetooth/BluetoothConstants.java
+++ messages/src/main/java/com/jwoglom/pumpx2/pump/messages/bluetooth/BluetoothConstants.java
@@ -3,7 +3,11 @@ package com.jwoglom.pumpx2.pump.messages.bluetooth;
 public final class BluetoothConstants {
     public final static String DEVICE_NAME_TSLIM_X2 = "tslim X2";
 
+    // note, the Bluetooth DIS device name still identifies as a X2.
+    // Only the user-visible name is labeled as "Tandem Mobi"
+    public final static String DEVICE_NAME_TANDEM_MOBI = "Tandem Mobi";
+
     public static boolean isTandemBluetoothDevice(String deviceName) {
-        return deviceName.startsWith(DEVICE_NAME_TSLIM_X2);
+        return deviceName.startsWith(DEVICE_NAME_TSLIM_X2) || deviceName.startsWith(DEVICE_NAME_TANDEM_MOBI);
     }
 }
